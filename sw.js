6d6b119018c130508d2c7a0f6e73cc80bfe3feb9
auco1120 <auco1120@outlook.com>
2020-12-13 15:24:04 -0600

    refactorización SW

--- sw.js
+++ sw.js
@@ -15,31 +15,10 @@ urlsToCache=[
 self.addEventListener('install', e => {
     evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-          console.log('[ServiceWorker] Pre-caching offline page');
+          console.log('Obteniendo pagina offline');
           return cache.addAll(urlsToCache);
         })
     );
-
-    evt.waitUntil(
-        caches.keys().then((keyList) => {
-          return Promise.all(keyList.map((key) => {
-            if (key !== CACHE_NAME) {
-              console.log('[ServiceWorker] Removing old cache', key);
-              return caches.delete(key);
-            }
-          }));
-        })
-    );
-    
-    //antiguo code con fallos, no funciona offline
-    /*e.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(CACHE => {
-            return caches.addAll(urlsToCache)
-            .then(() => self.skipWaiting())
-        })
-        .catch(err => console.warn('Fallo registro de cache', err))
-    )*/
 });
 
 self.addEventListener('activate', e => {
