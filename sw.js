d9f5781d8c5439c3d9905e47410428ba5f3df239
auco1120 <auco1120@outlook.com>
2020-12-13 15:06:20 -0600

    Tercer commit

--- sw.js
+++ sw.js
@@ -14,21 +14,15 @@ urlsToCache=[
 
 self.addEventListener('install', e => {
     evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-          console.log('[ServiceWorker] Pre-caching offline page');
-          return cache.addAll(urlsToCache);
+        caches.keys().then((keyList) => {
+          return Promise.all(keyList.map((key) => {
+            if (key !== CACHE_NAME) {
+              console.log('[ServiceWorker] Removing old cache', key);
+              return caches.delete(key);
+            }
+          }));
         })
     );
-    
-    
-    /*e.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(CACHE => {
-            return caches.addAll(urlsToCache)
-            .then(() => self.skipWaiting())
-        })
-        .catch(err => console.warn('Fallo registro de cache', err))
-    )*/
 });
 
 self.addEventListener('activate', e => {
